032a89fa3988f16d2cf92d5aee04dce643df145d
Tonderai Mutanda <tonderaimutanda@gmail.com>
2018-07-15 21:35:13 +0200

    Pet finder app, first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import fetchJsonp from 'fetch-jsonp';
+
+const petForm = document.querySelector('#pet-form');
+
+petForm.addEventListener('submit', fetchAnimals);
+
+// Fetc animals from api
+function fetchAnimals(e){
+    e.preventDefault();
+
+    // Get user input
+    const animal = document.querySeleto
+}
